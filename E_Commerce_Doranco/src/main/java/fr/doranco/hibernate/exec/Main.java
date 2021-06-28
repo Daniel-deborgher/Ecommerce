fa1d560f325ac9c4bbb6611422a96e2d8d50a6b8
Daniel-deborgher <64898999+Daniel-deborgher@users.noreply.github.com>
2021-06-28 16:55:34 +0200

    Add files via upload
    Mise à jour du projet, entité, pom, hibernate, main

--- E_Commerce_Doranco/src/main/java/fr/doranco/hibernate/exec/Main.java
+++ E_Commerce_Doranco/src/main/java/fr/doranco/hibernate/exec/Main.java
@@ -1,10 +1,39 @@
 package fr.doranco.hibernate.exec;
 
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+
+
+import fr.doranco.ecommerce.entity.Utilisateur;
+import fr.doranco.ecommerce.utils.Dates;
+import fr.doranco.ecommerce.utils.HibernateUtil;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+
+	public static void main(String[] args) throws Exception {
+			Utilisateur	 utilisateur1 = new Utilisateur("Daniel", "D", 
+					Dates.convertStringToDateUtil("11/12/1994"), false, 
+					"Mr", "d.daniel@gmail.com", "123456789", "0102030405");
+			
+			Transaction transaction = null;
+			Session session = null;
+			try {
+				session = HibernateUtil.getSessionFactory().openSession()
+				transaction = session.beginTransaction();
+				session.save(utilisateur1);
+				transaction.commit();
+			} catch(Exception e) {
+				if(transaction != null) {
+					transaction.rollback();
+				}
+				e.printStackTrace();
+			} finally {
+				if (session 
+			}
 	}
+	
 
 }
