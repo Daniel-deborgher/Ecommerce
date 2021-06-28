6046871e85145f490980550b23eadf47a352c9c2
Daniel deborgher <deborgher.daniel@gmail.com>
2021-06-28 16:52:43 +0200

    mise à jour entity

---
+++ src/main/java/fr/doranco/ecommerce/entity/Utilisateur.java
@@ -0,0 +1,118 @@
+package fr.doranco.ecommerce.entity;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "utilisateur")
+public class Utilisateur {
+	
+	@Id
+	private Integer id;
+	
+	@Column
+	private String nom;
+	private String prenom;
+	private Date dateNaissance;
+	private Boolean isActif;
+	private String profil;
+	private String email;
+	private String password;
+	private String telephone;
+	private Set<Adresse> adresses;
+	
+	public Utilisateur() {
+		adresses = new HashSet<Adresse>();
+	}
+	
+	
+	public Utilisateur(String nom, String prenom, Date dateNaissance, Boolean isActif, String profil, String email,
+			String password, String telephone) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.dateNaissance = dateNaissance;
+		this.isActif = isActif;
+		this.profil = profil;
+		this.email = email;
+		this.password = password;
+		this.telephone = telephone;
+	}
+
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public Date getDateNaissance() {
+		return dateNaissance;
+	}
+	public void setDateNaissance(Date dateNaissance) {
+		this.dateNaissance = dateNaissance;
+	}
+	public Boolean getIsActif() {
+		return isActif;
+	}
+	public void setIsActif(Boolean isActif) {
+		this.isActif = isActif;
+	}
+	public String getProfil() {
+		return profil;
+	}
+	public void setProfil(String profil) {
+		this.profil = profil;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public Set<Adresse> getAdresses() {
+		return adresses;
+	}
+
+	public void setAdresses(Set<Adresse> adresses) {
+		this.adresses = adresses;
+	}
+
+	@Override
+	public String toString() {
+		return "Utilisateur [id=" + id + ", nom=" + nom + ", prenom=" + prenom + ", dateNaissance=" + dateNaissance
+				+ ", isActif=" + isActif + ", profil=" + profil + ", email=" + email + ", password=" + password
+				+ ", telephone=" + telephone + ", adresses=" + adresses + "]";
+	}
+
+}
---
+++ src/main/java/fr/doranco/ecommerce/utils/HibernateUtil.java
@@ -0,0 +1,45 @@
+package fr.doranco.ecommerce.utils;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.metamodel.Metadata;
+import org.hibernate.metamodel.MetadataSources;
+
+public class HibernateUtil {
+	private static StandardServiceRegistry registry;
+	private static SessionFactory sessionFactory;
+	
+	public static SessionFactory getSessionFactory() {
+		if(sessionFactory == null) {
+			try {
+			// On créé un registry
+			registry = new StandardServiceRegistryBuilder().configure().build();
+			
+			// On créé un MetadataSources
+			MetadataSources sources = new MetadataSources(registry);
+			
+			// On créé un Metadata
+			Metadata metadata = sources.getMetadataBuilder().build();
+			
+			// On créé un SessionFactory
+			sessionFactory = metadata.getSessionFactoryBuilder().build();
+			
+			}
+			catch (Exception e){
+				e.printStackTrace();
+				if(registry != null) {
+					StandardServiceRegistryBuilder.destroy(registry);
+				}
+			}
+			
+		}
+		return sessionFactory;
+	}
+	public static void shutdown() {
+		if(registry != null) {
+			StandardServiceRegistryBuilder.destroy(registry);
+		}
+	}
+
+}
